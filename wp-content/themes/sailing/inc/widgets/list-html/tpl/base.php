acddab289a4ed1a06848578bb3699db87960fc37
dung.ngo <ngoducdung95@gmail.com>
2017-09-07 20:11:52 +0700

    Fix break layout : gioi thieu page

--- wp-content/themes/sailing/inc/widgets/list-html/tpl/base.php
+++ wp-content/themes/sailing/inc/widgets/list-html/tpl/base.php
@@ -1,7 +1,7 @@
 <?php
-echo '<div class="list-html-content row">';
+echo '<div class="list-html-content new-list-html-content">';
 foreach ( $instance['list-html'] as $i => $list_html ) {
-	echo '<div class="col-sm-6"><div class="item-content">';
+	echo '<div class="col-sm-6 new-col-item"><div class="item-content">';
 	echo '<div class="title-list"><h5>' . ( $i + 1 ) . '. ' . $list_html['title'] . ' </h5></div>';
 	echo '<div class="desc-list">' . $list_html['content'] . '</div>';
 
